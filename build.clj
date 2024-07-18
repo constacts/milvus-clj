42e1d82fb74f575818c7047811c49debf42df3f4
Todd Eunmin Kim <eunmin@constacts.com>
2024-07-18 22:33:08 +0900

    Release 0.2.8

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.constacts/milvus-clj)
-(def version "0.2.7")
+(def version "0.2.8")
 #_; alternatively, use MAJOR.MINOR.COMMITS:
   (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
